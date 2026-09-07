20425bca765637509a338129ee6cf8efcc0e13cf
Dave Rowbotham <thedaver64@gmail.com>
2026-09-07 12:39:32 +0100

    refactor include paths and move source files to applications/ and engine/ directories

--- include/sl.h
+++ include/sl.h
@@ -1,5 +1,19 @@
 #pragma once
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
 
+#include <atomic>
+#include <cstdarg>
+#include <cstdio>
+
+#include "audio.h"
+#include "display.h"
+#include "draw.h"
+#include "font.h"
+#include "gui.h"
+#include "system.h"
+
+#include <imgui.h>
 #include <cstdint>
 #include <string>
 
--- src/applications/game/game.h
+++ src/applications/game/game.h
@@ -1,20 +1,7 @@
 #pragma once
 
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_ttf.h>
+#include "sl.h"
 
-#include <atomic>
-#include <cstdarg>
-#include <cstdio>
-
-#include "audio.h"
-#include "display.h"
-#include "draw.h"
-#include "font.h"
-#include "gui.h"
-#include "system.h"
-
-#include <imgui.h>
 
 namespace game {
 
