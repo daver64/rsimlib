0fba13cde34d6e44cf5a61f55244b41c7e6e2568
Dave Rowbotham <thedaver64@gmail.com>
2026-09-08 10:24:39 +0100

    added triangle example

--- src/applications/triangle/extriangle.cpp
+++ src/applications/triangle/extriangle.cpp
@@ -6,7 +6,6 @@ void process_input()
     while (running)
     {
         SDL_Event event;
-        
         while (SDL_PollEvent(&event))
         {
             if (event.type == SDL_KEYDOWN || event.type==SDL_QUIT)
@@ -28,10 +27,30 @@ int main(int argc, char *argv[])
     simlib::Colour text_colour{0, 255, 0};
     simlib::clear_to_colour(simlib::screen, simlib::Colour{45, 48, 56});
 
-    simlib::gprintf_center(1 + fontheight, text_colour, "Hello, world!");
+    simlib::gprintf_center(32, text_colour, "Triangle drawing examples");
+
+    simlib::triangle(
+        simlib::screen, 100.0f, 210.0f, 220.0f, 80.0f, 340.0f, 210.0f,
+        simlib::Colour{255, 210, 60});
+    simlib::trianglefill(
+        simlib::screen, 460.0f, 210.0f, 580.0f, 80.0f, 700.0f, 210.0f,
+        simlib::Colour{60, 180, 255});
+
+    simlib::Bitmap *texture = simlib::load_bitmap("assets/textures/balloon_red.png");
+    if (texture)
+    {
+        simlib::triangle(
+            simlib::screen, 100.0f, 480.0f, 220.0f, 350.0f, 340.0f, 480.0f,
+            texture);
+        simlib::trianglefill(
+            simlib::screen, 460.0f, 480.0f, 580.0f, 350.0f, 700.0f, 480.0f,
+            texture);
+    }
+
     simlib::show_video_bitmap();
     simlib::end_frame();
     process_input();
+    simlib::destroy_bitmap(texture);
     simlib::shutdown();
     return 0;
 }
